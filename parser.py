149310f0ed011b42b03dcb81f9d1e7e43645d971
Nikhil-Narayanan <nikhilnar02@gmail.com>
2021-05-05 16:00:13 +0100

    started working on a data structure

--- parser.py
+++ parser.py
@@ -1,5 +1,19 @@
 #We want to make a data structure to store each line of the netlist
 
+class line():
+    def __init__(self, designator, node0, node1, value, node2=None):
+        if designator == "V":
+
+        elif designator == "I":
+
+        elif designator == "R":
+
+        elif designator == "C":
+
+        elif designator == ""
+
+
+
 def component_parser(line):
     line = line.split()
     #splits line into each word
@@ -7,3 +21,10 @@ def component_parser(line):
     #designator is the first letter of the first word
     node0 = line[1]
     node1 = line[2]
+    value = line[-1]
+    if len(line)==5:
+        node2 = line[3]
+    return line(designator, node0 , node1, node2, value)
+
+bob = component_parser(line)
+bob.ty
