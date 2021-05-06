bd7bb247008dc6c8b074aab3217faf2e14094b10
miguelbraganca <m.pl.braganca@gmail.com>
2021-05-06 09:59:42 +0100

    small signal classes

--- small_signal.py
+++ small_signal.py
@@ -1,4 +1,42 @@
 #Creates small signal equivalent of circuit
 
 #Make a class for each component
+class Vs:
+    def __init__(self, plus, minus, value):
+        self.plus = plus
+        self.minus = minus
+        self.value = value
+
+class Is:
+    def __init__(self, inp, out, value):
+        self.inp = inp
+        self.out = out
+        self.value = value
+
+class diode:
+    def __init__(self, anode, cathode, model):
+        self.anode = anode
+        self.cathode = cathode
+        self.model = model
+
+class bjt:
+    def __init__(self, collector, base, emitter, model):
+        self.collector = collector
+        self.base = base
+        self.emitter = emitter
+        self.model = model
+
+class res:
+    def __init__(self, value):
+        self.value = value
+
+class ind:
+    def __init__(self, value):
+        self.value = value
+
+class cap:
+    def __init__(self, value):
+        self.value = value
+
+
 #Make a data structure with the information of all the small signal parameters
